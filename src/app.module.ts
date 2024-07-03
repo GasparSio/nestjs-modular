74f923a21a369196f4f845d4eb0ce11a74ba5fd8
Gaspar Sio <sio.gaspar@gmail.com>
2024-07-03 09:12:18 +0200

    feat: Add HTTP service to fetch tasks from external API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { HttpService, HttpModule } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 const API_KEY = '123456';
 const API_KEY_PROD = 'PRODU1231291';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await firstValueFrom(
+          http.get('https://jsonplaceholder.typicode.com/todos'),
+        );
+        return tasks;
+      },
+      inject: [HttpService],
     },
   ],
 })
