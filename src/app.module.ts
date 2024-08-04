0f5b9201ecb6b1cb6002c50b2e6bd0efe5851c07
Gaspar Sio <sio.gaspar@gmail.com>
2024-08-04 14:36:51 +0200

    chore: Add joi npm dependency for input validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,20 @@ import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { environments } from './environments';
+import config from 'config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env', //decirle que archivo tiene que leer
       isGlobal: true, //hacerlo global para todo el proyecto
+      load: [config],
+      validationSchema: Joi.object({
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        API_KEY: Joi.string().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
+import { ConfigType } from '@nestjs/config';
+import config from 'config';
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY');
-    const database = this.configService.get('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const database = this.configService.database.name;
     return `Hello World!, we are using the API KEY: ${apiKey} and the Database ${database}`;
   }
 }
